989cf3af4fad263c0c1433dbfeefc74206f4ecc7
JUDECHIHAN J. <judechihan727@gmail.com>
2026-04-02 09:57:35 +0530

    Initial project setup and setup design styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/*Theme toggle*/}
+
+
+        {/*Background effects*/}
+
+
+        {/*Navbar*/}
+
+
+        {/*Main content*/}
+
+
+        {/*Footer */}
+
+    </div>
+  )
+}
